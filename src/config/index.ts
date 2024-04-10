72ffd2f83926dc7d15aae09da73b1034994f956f
ezeq10 <eclipsejem@gmail.com>
2024-04-10 13:47:50 -0300

    changed config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+export const API_URL = "https://api.football-data.org/v4";
+export const ALLOWED_API_REQUESTS_PER_DAY = 20;
+
 export const { 
   MONGODB_URI,
-  API_URL,
   API_TOKEN,
-  ALLOWED_API_REQUESTS_PER_DAY,
 } = process.env;
